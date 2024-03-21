b0bee91596a0a421011bee15a8ef9ba7ffa51e1c
@RupertoTacuche <murcio2490@gmail.com>
2024-03-20 22:41:15 -0600

    clase_447

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -23,4 +23,15 @@ param('id').isMongoId().withMessage('ID no valido'),
 handleInputErrors,
 ProjectController.getProjectById )
 
+router.put('/:id', 
+param('id').isMongoId().withMessage('ID no valido'),
+body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+body('description')
+    .notEmpty().withMessage('La descripcion del proyecto es obligatoria'),
+handleInputErrors,
+ProjectController.updateProject )
+
 export default router
